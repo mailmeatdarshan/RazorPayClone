a18d986004328fcc734669b020d975b5cc9a5c53
Darshan Dubey <mailmeatdarshan@gmail.com>
2025-05-19 12:20:43 +0530

    changed code

--- main.js
+++ main.js
@@ -1,32 +1,39 @@
-let login = document.getElementById('loginBtn');
-let signup = document.getElementById('signupBtn');
-let signupnow = document.getElementById('signupnowBtn');
-const menuLinks = document.querySelectorAll('#navMenu a');
+document.addEventListener('DOMContentLoaded', function() {
+  let login = document.getElementById('loginBtn');
+  let signup = document.getElementById('signupBtn');
+  let signupnow = document.getElementById('signupnowBtn');
+  const menuLinks = document.querySelectorAll('#navMenu a');
 
-const commonLink = "https://razorpay.com/";
+  const commonLink = "https://razorpay.com/";
 
-menuLinks.forEach(link => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault();
-    alert("you are redirecting to original website");
-    window.location.href = commonLink; 
-  });
+  if (menuLinks.length > 0) {
+    menuLinks.forEach(link => {
+      link.addEventListener('click', (event) => {
+        event.preventDefault();
+        alert("you are redirecting to original website");
+        window.location.href = commonLink; 
+      });
+    });
+  }
+
+  if (login) {
+    login.addEventListener('click', () => {
+        alert("you are redirecting to login page");
+        window.location.href = 'https://accounts.razorpay.com/auth/?redirecturl=https%3A%2F%2Fdashboard.razorpay.com&auth_intent=login&x-country-code=IN';
+    });
+  }
+
+  if (signup) {
+    signup.addEventListener('click', () => {
+        alert("you are redirecting to signup page");
+        window.location.href = 'https://accounts.razorpay.com/auth/?redirecturl=https%3A%2F%2Feasy.razorpay.com&auth_intent=signup&x-country-code=IN'
+    });
+  }
+
+  if (signupnow) {
+    signupnow.addEventListener('click', () => {
+        alert("you are redirecting to signup page");
+        window.location.href = 'https://accounts.razorpay.com/auth/?redirecturl=https%3A%2F%2Feasy.razorpay.com&auth_intent=signup&x-country-code=IN'
+    });
+  }
 });
-
-
-login.addEventListener('click', () => {
-    alert("you are redirecting to login page");
-    window.location.href = 'https://accounts.razorpay.com/auth/?redirecturl=https%3A%2F%2Fdashboard.razorpay.com&auth_intent=login&x-country-code=IN';
-});
-
-signup.addEventListener('click', () => {
-    alert("you are redirecting to signup page");
-    window.location.href = 'https://accounts.razorpay.com/auth/?redirecturl=https%3A%2F%2Feasy.razorpay.com&auth_intent=signup&x-country-code=IN'
-});
-
-signupnow.addEventListener('click', () => {
-    alert("you are redirecting to signup page");
-    window.location.href = 'https://accounts.razorpay.com/auth/?redirecturl=https%3A%2F%2Feasy.razorpay.com&auth_intent=signup&x-country-code=IN'
-});
-
-
